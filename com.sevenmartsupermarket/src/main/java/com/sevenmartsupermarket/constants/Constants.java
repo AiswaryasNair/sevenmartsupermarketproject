0e59fcd32f49c6a994ce69bf7f09cb9e37ffb368
ammua <ammua@AMMU21515>
2024-05-24 20:55:25 +0530

    committing constant class

--- com.sevenmartsupermarket/src/main/java/com/sevenmartsupermarket/constants/Constants.java
+++ com.sevenmartsupermarket/src/main/java/com/sevenmartsupermarket/constants/Constants.java
@@ -8,5 +8,4 @@ public class Constants {
 	public static final String EXCEL_FILE_PATH=System.getProperty("user.dir")+"\\src\\main\\resources\\ExcelFiles\\";
 	public static final String SCREENSHOT_FILE_PATH=System.getProperty("user.dir")+"\\Screenshots\\";
 	public static final String EXTENT_REPORT_PATH=System.getProperty("user.dir")+"\\ExtentReport";
-	
 }
